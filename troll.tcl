bfd096b0222c30dde72f05bdcabf2f71f78e20e2
Stathis Xantinidis <spithash@tuta.io>
2024-07-19 09:37:17 +0300

    added debugging info

--- troll.tcl
+++ troll.tcl
@@ -40,31 +40,34 @@ set delay 10
 bind pub - !troll parse
 
 proc parse {nick uhost hand chan text} {
-global delay
-variable troll
-if {![channel get $chan troll]} {
-    return 0
-}
-if {[info exists troll(lasttime,$chan)] && [expr $troll(lasttime,$chan) + $delay] > [clock seconds]} {
-    putserv "NOTICE $nick :You can use only 1 command in $delay seconds. Wait [expr $delay - [expr [clock seconds] - $troll(lasttime,$chan)]] seconds and try again, nigga."
-    return 0
-}
-http::register https 443 [list ::tls::socket -autoservername true]
-::http::config -urlencoding utf-8 -useragent "Mozilla/5.0 (X11; U; Linux i686; el-GR; rv:1.8.1) Gecko/2010112223 Firefox/3.6.12"
-set url [::http::geturl "https://fnet.gr/nou/meow.php" -timeout 15000]
-set data [::http::data $url]
-::http::cleanup $url
+    global delay
+    variable troll
+    if {![channel get $chan troll]} {
+        return 0
+    }
+    if {[info exists troll(lasttime,$chan)] && [expr $troll(lasttime,$chan) + $delay] > [clock seconds]} {
+        putserv "NOTICE $nick :You can use only 1 command in $delay seconds. Wait [expr $delay - [expr [clock seconds] - $troll(lasttime,$chan)]] seconds and try again."
+        return 0
+    }
+    http::register https 443 [list ::tls::socket -autoservername true]
+    ::http::config -urlencoding utf-8 -useragent "Mozilla/5.0 (X11; U; Linux i686; el-GR; rv:1.8.1) Gecko/2010112223 Firefox/3.6.12"
+    set url [::http::geturl "https://fnet.gr/nou/meow.php" -timeout 15000]
+    set data [::http::data $url]
+    ::http::cleanup $url
 
-regsub -all -- {\n} $data " " data;
-regexp -nocase -- {<p .*?>(.*?)</p>} $data -> info
-regsub -all -- {(<strong[^>]*>)|(</strong>)} $info "\002" info;
+    regsub -all -- {\n} $data " " data
+    if {[regexp -nocase -- {<p .*?>(.*?)</p>} $data -> info]} {
+        regsub -all -- {(<strong[^>]*>)|(</strong>)} $info "\002" info
 
-while {$info != ""} {
-    putserv "PRIVMSG $chan :[string range $info 0 419]"
-    set info [string range $info 420 end]
-}
-set troll(lasttime,$chan) [clock seconds]
-
+        while {$info != ""} {
+            putserv "PRIVMSG $chan :[string range $info 0 419]"
+            set info [string range $info 420 end]
+        }
+        set troll(lasttime,$chan) [clock seconds]
+    } else {
+        putserv "NOTICE $nick :Error: No matching data found."
+        putlog "DEBUG: Data fetched: $data"
+    }
 }
 putlog "\002troll.tcl\002 v2.5 by spithash@Libera iz up and trollin'"
 # EOF
