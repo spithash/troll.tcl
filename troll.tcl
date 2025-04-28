2defb8a8bf18fcd24a4e53268c1e2845b67d5b80
Stathis Xantinidis <spithash@tuta.io>
2025-04-28 10:29:26 +0000

    changed db url & removed extra space in the bot's reply
    also handling the bolding better

--- troll.tcl
+++ troll.tcl
@@ -1,5 +1,5 @@
 #############################################################################################################
-# Troll v2.5.1 TCL by spithash@Libera                                                                       #
+# Troll v2.8 TCL by spithash@Libera                                                                         #
 #############################################################################################################
 # Gets troll quotes and makes people suffer!                                                                #
 #############################################################################################################
@@ -31,43 +31,76 @@
 package require http
 package require tls
 
-# Channel flag.
+# Channel flag for enabling the feature
 setudef flag troll
 
-# Set the time (in seconds) between commands.
+# Set the time (in seconds) between uses
 set delay 10
 
+# Configure HTTP to support HTTPS
+http::register https 443 [list ::tls::socket -autoservername true]
+::http::config -urlencoding utf-8 -useragent "Mozilla/5.0 (X11; U; Linux i686; el-GR; rv:1.8.1) Gecko/2010112223 Firefox/3.6.12"
+
+# Bind the !troll command
 bind pub - !troll parse
 
 proc parse {nick uhost hand chan text} {
     global delay
     variable troll
+
     if {![channel get $chan troll]} {
         return 0
     }
-    if {[info exists troll(lasttime,$chan)] && [expr $troll(lasttime,$chan) + $delay] > [clock seconds]} {
-        putserv "NOTICE $nick :You can use only 1 command in $delay seconds. Wait [expr $delay - [expr [clock seconds] - $troll(lasttime,$chan)]] seconds and try again."
+
+    if {[info exists troll(lasttime,$chan)] && ([clock seconds] - $troll(lasttime,$chan)) < $delay} {
+        set wait [expr {$delay - ([clock seconds] - $troll(lasttime,$chan))}]
+        putserv "NOTICE $nick :You can use only 1 command every $delay seconds. Wait $wait more second(s)."
         return 0
     }
-    http::register https 443 [list ::tls::socket -autoservername true]
-    ::http::config -urlencoding utf-8 -useragent "Mozilla/5.0 (X11; U; Linux i686; el-GR; rv:1.8.1) Gecko/2010112223 Firefox/3.6.12"
-    set url [::http::geturl "https://fnet.gr/nou/meow.php" -timeout 15000]
-    set data [::http::data $url]
-    ::http::cleanup $url
 
-    regsub -all -- {\n} $data " " data
+    set url "http://meoww.getenjoyment.net/nou.php"
+
+    # Fetch URL
+    set token [::http::geturl $url -timeout 15000]
+    if {[::http::status $token] ne "ok"} {
+        putserv "NOTICE $nick :Error fetching data. HTTP status: [::http::status $token]"
+        ::http::cleanup $token
+        return 0
+    }
+
+    set data [::http::data $token]
+    ::http::cleanup $token
+
+    # Collapse all whitespace (spaces, tabs, newlines) into single spaces
+    regsub -all -- {\s+} $data " " data
+    set data [string trim $data]
+
+    # Extract content inside <p>...</p>
     if {[regexp -nocase -- {<p .*?>(.*?)</p>} $data -> info]} {
-        regsub -all -- {(<strong[^>]*>)|(</strong>)} $info "\002" info
+        set info [string trim $info]
 
-        while {$info != ""} {
+        # Replace <strong> and <b> tags with bold control code (\002)
+        regsub -all -- {(<strong[^>]*>|<b[^>]*>)} $info "\002" info
+        regsub -all -- {</strong>|</b>} $info "\002" info
+
+        # Strip any other remaining HTML tags
+        regsub -all -- {<[^>]+>} $info "" info
+
+        set info [string trim $info]
+
+        # Send output in chunks of 420 characters
+        while {[string length $info] > 0} {
             putserv "PRIVMSG $chan :[string range $info 0 419]"
             set info [string range $info 420 end]
         }
+
+        # Update lasttime
         set troll(lasttime,$chan) [clock seconds]
     } else {
         putserv "NOTICE $nick :Error: No matching data found."
         putlog "DEBUG: Data fetched: $data"
     }
 }
-putlog "\002troll.tcl\002 v2.5.1 by spithash@Libera iz up and trollin'"
+
+putlog "\002troll.tcl\002 v2.8 by spithash@Libera is up and trollin'!"
 # EOF
